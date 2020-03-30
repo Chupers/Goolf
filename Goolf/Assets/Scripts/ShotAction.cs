630275912979767f1ecc51af167d58609c4c04fd
Eugenie Chupris <zhenuh15@gmail.com>
2020-03-30 16:36:18 +0300

    init Project and Force push

---
+++ Goolf/Assets/Scripts/ShotAction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotAction : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float forse = 50f;
+    public TrajectoryRenderer trajectoryRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetInput())
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                
+                Vector3 targetPosition = hitInfo.point;
+                Vector3 direction = rigidbody.transform.position - targetPosition * forse;
+                trajectoryRenderer.ShowTrajectory(transform.position, direction);
+                rigidbody.AddForce(direction.normalized, ForceMode.VelocityChange);
+            }
+        }
+    }
+    private bool GetInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Goolf/Assets/Scripts/TrajectoryRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrajectoryRenderer : MonoBehaviour
+{
+    public GameObject gameObject;
+
+    private LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    public void ShowTrajectory(Vector3 vector, Vector3 direction)
+    {
+        GameObject gameObject = Instantiate(this.gameObject, vector, Quaternion.identity);
+        gameObject.GetComponent<Rigidbody>().AddForce(direction.normalized, ForceMode.VelocityChange);
+
+        Physics.autoSimulation = false;
+
+        //simulation
+
+        Vector3[] points = new Vector3[100];
+
+        for(int i = 0; i < points.Length;i++)
+        {
+            Physics.Simulate(0.1f);
+            points[i] = gameObject.transform.position;
+        }
+
+        lineRenderer.SetPositions(points);
+
+        Physics.autoSimulation = true;
+
+        Destroy(gameObject);
+
+    }
+}
